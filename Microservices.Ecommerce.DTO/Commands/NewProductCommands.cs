c37dcc71522e9e87d85aa3a5a0415337540a67c1
BolzanLuca <83642308+BolzanLuca@users.noreply.github.com>
2021-12-09 16:36:36 +0100

    crud commandi ordini

---
+++ Microservices.Ecommerce.DTO/Commands/NewProductCommands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservices.Ecommerce.DTO.Commands
+{
+    public class NewProductCommands
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public double Aliquota { get; set; }
+
+        public string Marca { get; set; }
+
+        public double Prezzo { get; set; }
+    }
+}
