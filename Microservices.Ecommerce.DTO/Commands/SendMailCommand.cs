88ecd0dd40e2054f3962c50772de75f7526012a9
Saliou <gayesaliou29@gmail.com>
2021-12-16 15:29:16 +0100

    Add Mail Event

---
+++ Microservices.Ecommerce.DTO/Commands/SendMailCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Microservices.Ecommerce.DTO.Commands
+{
+    public class SendMailCommand
+    {
+        public String To { get; set; }
+        public String Cc { get; set; }
+        public String Ccn { get; set; }
+        public String Subject { get; set; }
+        public String Body { get; set; }
+    }
+}
